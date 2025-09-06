2f17df01971f994243ea4d86f52d643cd8314ff7
Roberto <1135584@atitus.edu.br>
2025-09-05 23:53:30 -0300

    Feat: Testes adicionado - não pode animal duplicado/Pessoa 1 não pode adotar  gatos com brinquedo duplicado

--- src/abrigo-animais.test.js
+++ src/abrigo-animais.test.js
@@ -11,32 +11,64 @@ describe('Abrigo de Animais', () => {
   test('Deve encontrar pessoa para um animal', () => {
     const resultado = new AbrigoAnimais().encontraPessoas(
       'RATO,BOLA', 'RATO,NOVELO', 'Rex,Fofo');
-      expect(resultado.lista[0]).toBe('Fofo - abrigo');
-      expect(resultado.lista[1]).toBe('Rex - pessoa 1');
-      expect(resultado.lista.length).toBe(2);
-      expect(resultado.erro).toBeFalsy();
+    expect(resultado.lista[0]).toBe('Fofo - abrigo');
+    expect(resultado.lista[1]).toBe('Rex - pessoa 1');
+    expect(resultado.lista.length).toBe(2);
+    expect(resultado.erro).toBeFalsy();
   });
 
   test('Deve encontrar pessoa para um animal intercalando brinquedos', () => {
     const resultado = new AbrigoAnimais().encontraPessoas('BOLA,LASER',
       'BOLA,NOVELO,RATO,LASER', 'Mimi,Fofo,Rex,Bola');
 
-      expect(resultado.lista[0]).toBe('Bola - abrigo');
-      expect(resultado.lista[1]).toBe('Fofo - pessoa 2');
-      expect(resultado.lista[2]).toBe('Mimi - abrigo');
-      expect(resultado.lista[3]).toBe('Rex - abrigo');
-      expect(resultado.lista.length).toBe(4);
-      expect(resultado.erro).toBeFalsy();
+    expect(resultado.lista[0]).toBe('Bola - abrigo');
+    expect(resultado.lista[1]).toBe('Fofo - pessoa 2');
+    expect(resultado.lista[2]).toBe('Mimi - abrigo');
+    expect(resultado.lista[3]).toBe('Rex - abrigo');
+    expect(resultado.lista.length).toBe(4);
+    expect(resultado.erro).toBeFalsy();
   });
   test('Pessoa já adotou antes e depois tenta adotar o Loco', () => {
     const resultado = new AbrigoAnimais().encontraPessoas('BOLA,LASER',
-    'BOLA,NOVELO,RATO,LASER', 'Loco')
-    expect(resultado.lista[0]).toBe("Loco - abrigo"); 
+      'BOLA,NOVELO,RATO,LASER', 'Loco')
+    expect(resultado.lista[0]).toBe("Loco - abrigo");
   })
 
-    test("deve retornar erro para brinquedo inválido", () => {
+  test("deve retornar erro para brinquedo inválido", () => {
     const resultado = new AbrigoAnimais().encontraPessoas("BOLA,INVALIDO", "NOVELO", "Rex");
-     expect(resultado.erro).toBe("Brinquedo inválido");;
+    expect(resultado.erro).toBe("Brinquedo inválido");;
   });
 
+  test('Deve mandar animal pro abrigo quando os dois conseguem adotar', () => {
+    const resultado = new AbrigoAnimais().encontraPessoas('RATO,BOLA', 'RATO,BOLA', 'Rex');
+    expect(resultado.lista[0]).toBe('Rex - abrigo');
+    expect(resultado.erro).toBeFalsy();
+  });
+
+
+
+    test('Loco sozinho segue regra normal', () => {
+    const resultado = new AbrigoAnimais().encontraPessoas('SKATE,RATO', 'LASER', 'Loco');
+    expect(resultado.lista[0]).toBe('Loco - pessoa 1');
+    expect(resultado.lista.length).toBe(1);
+    expect(resultado.erro).toBeFalsy();
+  });
+
+  test ('Não pode adotar animal duplicad', () => {
+    const resultado = new AbrigoAnimais().encontraPessoas('RATO,BOLA', 'RATO,BOLA', 'Rex,Rex')
+    expect(resultado.erro).toBe('Animal inválido' )
+  })
+
+    test('Pessoa 1 não pode adotar dois gatos com brinquedo repetido', () => {
+    const resultado = new AbrigoAnimais().encontraPessoas(
+      'BOLA,LASER,RATO',
+      'NOVELO',
+      'Mimi,Fofo'
+    );
+
+    expect(resultado.lista[0]).toBe('Fofo - abrigo');
+    expect(resultado.lista[1]).toBe('Mimi - pessoa 1');
+    expect(resultado.lista.length).toBe(2);
+    expect(resultado.erro).toBeFalsy();
+  });
 });
